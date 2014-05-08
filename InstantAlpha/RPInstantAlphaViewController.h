a0c5c9ae3e3d5a21f1b5f6624c41122261a46c47
Brandon Evans <brandon@brandonevans.ca>
2014-05-07 18:44:41 -0600

    Remove todo list comment

--- InstantAlpha/RPInstantAlphaViewController.h
+++ InstantAlpha/RPInstantAlphaViewController.h
@@ -6,17 +6,6 @@
 //  Copyright (c) 2014 Robots and Pencils. All rights reserved.
 //
 
-// TODO:
-//
-// [X] Should be able to create with an image and callback
-// [X] Should display provided image
-// [X] Should display instructions with reset and done buttons
-// [ ] Should display loupe that follows cursor when inside view
-// [X] Should allow clicking to select the target color
-// [X] Should highlight the target color range
-// [X] Should allow dragging the selection to set a threshold
-// [X] Should draw with masked colors when mouse is released
-
 @class RPThresholdLabelView;
 
 @interface RPInstantAlphaViewController : NSViewController
